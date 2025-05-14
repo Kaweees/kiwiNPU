ffd91ace3d327a07e4348565535b3c5629a1ef99
Miguel Villa Floran <miguel.villafloran@gmail.com>
2025-05-14 16:52:51 -0700

    Update shell.nix

--- shell.nix
+++ shell.nix
@@ -5,7 +5,14 @@ pkgs.mkShell {
     pkgs.gnumake # GNU Make
     pkgs.verilator # Verilog simulator
     pkgs.iverilog # Verilog simulator
-    # pkgs.tput # GTKWave waveform viewer
+    pkgs.ncurses # tput (terminal formatting)
+    pkgs.findutils # find command
+    pkgs.gnugrep # grep
+    pkgs.coreutils # basic Unix commands
+    pkgs.which # which command
+    # Uncomment if you need OpenLane/OpenROAD
+    pkgs.openroad # OpenROAD
+    pkgs.openlane # OpenLane flow
   ];
 
   # Shell hook to set up environment
